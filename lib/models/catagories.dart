8da8da48e739d526b47636cf465bcb4fb92cabd8
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-15 18:45:09 +0530

    Update catagories.dart

--- lib/models/catagories.dart
+++ lib/models/catagories.dart
@@ -3,6 +3,9 @@ class Catagory{
   final String id;
   final String tittle;
   final Color color;
+  /*CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740*/
   const Catagory({
     @required this.id,
     @required this.tittle,
